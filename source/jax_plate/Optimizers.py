1. Switched to float64 2. Added correction for the Accelerometer mass 3. Fixed bug in interpolation 4. Added batched computation of AFC (needs testing) 5. Switched to HCT finite elements (tentative)

source/jax_plate/Optimizers.py
@@ -19,8 +19,6 @@ def get_sd_and_norm(
 
 def solve_trust_region_model(B, g, delta, rtol=1e-6, max_iter=100):
     lams, eigenvectors = np.linalg.eigh(B)
-    print(lams)
-    print(eigenvectors @ g / np.linalg.norm(g))
     sd = jnp.linalg.solve(B, -g)
     pnorm = jnp.linalg.norm(sd)
     if pnorm <= delta:
@@ -71,7 +69,6 @@ def solve_trust_region_model(B, g, delta, rtol=1e-6, max_iter=100):
         predicted_improvement >= 0
     ), "Predicted improvement for quadratic model is negative"
 
-    print(m, k)
     return sd, lam, predicted_improvement
 
 
@@ -168,7 +165,7 @@ def optimize_trust_region(
         x_history.append(x)
         grad_history.append(g)
 
-        print(delta)
+        # print(delta)
 
         if cur_f < 1e-7:
             status = "Converged"

source/jax_plate/Problem.py
@@ -5,6 +5,10 @@ import jax
 import jax.numpy as jnp
 import jax.scipy as jsp
 
+from jax.config import config
+
+config.update("jax_enable_x64", True)
+
 from .pyFFInterface import *
 
 
@@ -23,7 +27,11 @@ class Problem:
         """
 
     def __init__(
-        self, path_to_edp: str, thickness: np.float64, density: np.float64,
+        self,
+        path_to_edp: str,
+        thickness: np.float64,
+        density: np.float64,
+        accelerometer_param: dict = None,
     ):
         """Constructor method"""
         self.h = thickness
@@ -33,12 +41,19 @@ class Problem:
         processed_ff_output = processFFOutput(getOutput(path_to_edp))
 
         # Loading necessary data from model to GPU
-        self.Ks = jnp.array(processed_ff_output["Ks"])
+        self.Ks = jnp.array(processed_ff_output["Ks"], dtype=jnp.float64)
         self.fKs = jnp.array(processed_ff_output["fKs"])
         self.M = jnp.array(processed_ff_output["M"])
         self.fM = jnp.array(processed_ff_output["fM"])
         self.L = jnp.array(processed_ff_output["L"])
         self.fL = jnp.array(processed_ff_output["fL"])
+
+        # Correction of the inertia matrices by the mass of the accelerometer
+        # TODO get rid of copypasta programming
+        MCorrection = jnp.array(processed_ff_output["MCorrection"])
+        fMCorrection = jnp.array(processed_ff_output["fMCorrection"])
+        LCorrection = jnp.array(processed_ff_output["LCorrection"])
+        fLCorrection = jnp.array(processed_ff_output["fLCorrection"])
         # TODO load can be not in phase with the BC vibration
         # and both BC and load may have different phases in points
         # so both of them have to be complex in general
@@ -50,6 +65,20 @@ class Problem:
         # BC term
         self.fInertia = self.rho * (self.fM + 1.0 / 3.0 * self.e ** 2 * self.fL)
 
+        if accelerometer_param is not None:
+            # TODO: check e vs 2e=h
+            rho_corr = (
+                accelerometer_param["mass"]
+                / (np.pi * accelerometer_param["radius"] ** 2)
+                / self.e
+            )
+            self.MInertia += rho_corr * (
+                MCorrection + 1.0 / 3.0 * self.e ** 2 * LCorrection
+            )
+            self.fInertia += rho_corr * (
+                fMCorrection + 1.0 / 3.0 * self.e ** 2 * fLCorrection
+            )
+
         self.interpolation_vector = jnp.array(
             processed_ff_output["interpolation_vector"]
         )
@@ -60,7 +89,7 @@ class Problem:
         self.test_point = processed_ff_output["test_point_coord"]
         self.constrained_idx = processed_ff_output["constrained_idx"]
 
-    def getAFCFunction(self, params_to_physical):
+    def getAFCFunction(self, params_to_physical, batch_size=None):
         """Creates function to evaluate AFC 
             :param params_to_physical: Function that converts chosen model parameters to the physical parameters of the model,
                 D_ij storage modulus [Pa], beta_ij loss factor [1], ij in [11, 12, 16, 22, 26, 66], in total 12 parameters.
@@ -72,7 +101,6 @@ class Problem:
             """
 
         def _solve(f, params):
-            # Pseudocode of function, TODO: implement:D
             omega = 2.0 * np.pi * f
             e = self.e
             D, beta = params_to_physical(params)
@@ -95,25 +123,54 @@ class Problem:
             b_imag = fK_imag
 
             A = jnp.vstack(
-                (jnp.hstack((A_real, -A_imag)), jnp.hstack((A_imag, A_real)))
+                (jnp.hstack((A_real, -A_imag)), jnp.hstack((-A_imag, -A_real)))
             )
-            b = jnp.concatenate((b_real, b_imag))
-            x = jsp.linalg.solve(A, b)
+            b = jnp.concatenate((b_real, -b_imag))
 
-            u = (
-                self.interpolation_value_from_bc
-                + self.interpolation_vector @ x.reshape((-1, 2))
+            x = jsp.linalg.solve(A, b, check_finite=False)
+
+            u = jnp.array(
+                [self.interpolation_value_from_bc, 0.0]
+            ) + self.interpolation_vector @ x.reshape(  # real, imag
+                (-1, 2), order="F"
             )
 
             return u
 
-        return jax.jit(jax.vmap(_solve, in_axes=(0, None),))
+        _get_afc = jax.jit(jax.vmap(_solve, in_axes=(0, None),))
+        
+        if batch_size is None:
+            return _get_afc
 
-    def getMSELossFunction(self, params_to_physical, frequencies, reference_afc):
+        # Workaround to avoid memory error 
+        def _get_afc_batched(fs, params):
+            N_omega = fs.shape[0]
+            if batch_size >= N_omega:
+              return _get_afc(fs, params)
+            n_batches = (N_omega + batch_size - 1)//batch_size
+            afc = _get_afc(fs[:batch_size], params)
+            for i in range(1, n_batches - 1):
+                afc = jnp.vstack((afc, _get_afc(fs[i*batch_size:(i+1)*batch_size], params)))
+            i += 1
+            afc = jnp.vstack((afc, _get_afc(fs[i*batch_size:], params)))
+            return afc
+
+        return _get_afc_batched
+
+    def getSolutionMatrices(self, D, beta):
+        loss_moduli = beta * D
+        e = self.e
+
+        K_real = jnp.einsum(self.Ks, [0, ...], D, [0]) / 2.0 / e
+        K_imag = jnp.einsum(self.Ks, [0, ...], loss_moduli, [0]) / 2.0 / e
+
+        return K_real, K_imag, self.MInertia
+
+    def getMSELossFunction(self, params_to_physical, frequencies, reference_afc, batch_size=None):
         assert frequencies.shape[0] == reference_afc.shape[0]
         assert reference_afc.shape[1] == 2
 
-        afc_function = self.getAFCFunction(params_to_physical)
+        afc_function = self.getAFCFunction(params_to_physical, batch_size)
 
         def MSELoss(params):
             afc = afc_function(frequencies, params)

source/jax_plate/Utils.py
@@ -18,41 +18,41 @@ def isotropic_to_full(isotropic_params):
     return Ds, betas
 
 
-def plot_afc_radial(freqs, afc, label, fig, axs):
+def plot_afc_radial(freqs, afc, fig, axs, **line_kwargs):
 
     afc_module = jnp.linalg.norm(afc, axis=1, ord=2)
     afc_phase_shift = jnp.arctan2(afc[:, 0], afc[:, 1]) / jnp.pi
 
     axs[0].set_yscale("log")
-    axs[0].plot(freqs, afc_module, label=label)
+    axs[0].plot(freqs, afc_module, **line_kwargs)
     axs[0].set_title(r"$\|u\|$")
-    axs[0].set_xlabel('$f,\\ Hz$')
+    axs[0].set_xlabel("$f,\\ Hz$")
     axs[0].grid(True)
 
-    axs[1].plot(freqs, afc_phase_shift, label=label)
+    axs[1].plot(freqs, afc_phase_shift, **line_kwargs)
     axs[1].set_title(r"$\frac{\delta(\varphi)}{\pi}$")
     axs[1].grid(True)
-    axs[1].set_xlabel('$f,\\ Hz$')
+    axs[1].set_xlabel("$f,\\ Hz$")
     axs[1].legend()
     return fig, axs
 
 
-def plot_afc_complex(freqs, afc, label, fig, axs):
+def plot_afc_complex(freqs, afc, fig, axs, **line_kwargs):
 
-    axs[0].plot(freqs, afc[:, 0], label=label)
+    axs[0].plot(freqs, afc[:, 0], **line_kwargs)
     axs[0].set_title(r"$\Re(u)$")
-    axs[0].set_xlabel('$f,\\ Hz$')
+    axs[0].set_xlabel("$f,\\ Hz$")
     axs[0].grid(True)
 
-    axs[1].plot(freqs, afc[:, 1], label=label)
+    axs[1].plot(freqs, afc[:, 1], **line_kwargs)
     axs[1].set_title(r"$\Im(u)$")
-    axs[1].set_xlabel('$f,\\ Hz$')
+    axs[1].set_xlabel("$f,\\ Hz$")
     axs[1].grid(True)
     axs[1].legend()
     return fig, axs
 
 
-def plot_afc(freqs, afc, label, fig=None, kind="Radial"):
+def plot_afc(freqs, afc, fig=None, kind="Radial", **line_kwargs):
     if fig is None:
         if kind == "Radial":
             fig, axs = plt.subplots(figsize=(20, 10), nrows=1, ncols=2, sharex=True)
@@ -71,6 +71,6 @@ def plot_afc(freqs, afc, label, fig=None, kind="Radial"):
             kind = "Complex"
 
     if kind == "Radial":
-        return plot_afc_radial(freqs, afc, label, fig, axs)
+        return plot_afc_radial(freqs, afc, fig, axs, **line_kwargs)
     elif kind == "Complex":
-        return plot_afc_complex(freqs, afc, label, fig, axs)
+        return plot_afc_complex(freqs, afc, fig, axs, **line_kwargs)

source/jax_plate/pyFFInterface.py
@@ -19,16 +19,20 @@ def getOutput(fname: str):
     # ensure it in _problem.edp
     script += pyff.VarfScript(
         # Stiffness terms depending on each of the anisotropic coefs
-        K11="int2d(Th)(dxx(u)*dxx(v)) + on(1, u=0, ux=0, uy=0)",
-        K12="int2d(Th)(dyy(u)*dxx(v) + dxx(u)*dyy(v)) + on(1, u=0, ux=0, uy=0)",
-        K16="int2d(Th)(dxy(u)*dxx(v) + 2.*dxx(u)*dxy(v)) + on(1, u=0, ux=0, uy=0)",
-        K22="int2d(Th)(dyy(u)*dyy(v)) + on(1, u=0, ux=0, uy=0)",
-        K26="int2d(Th)(dxy(u)*dyy(v) + 2.*dyy(u)*dxy(v)) + on(1, u=0, ux=0, uy=0)",
-        K66="int2d(Th)(2.*dxy(u)*dxy(v)) + on(1, u=0, ux=0, uy=0)",
+        K11="int2d(Th, qft=qfHCT5)(dxx(u)*dxx(v)) + on(1, u=0, ux=0, uy=0)",
+        K12="int2d(Th, qft=qfHCT5)(dyy(u)*dxx(v) + dxx(u)*dyy(v)) + on(1, u=0, ux=0, uy=0)",
+        K16="int2d(Th, qft=qfHCT5)(dxy(u)*dxx(v) + 2.*dxx(u)*dxy(v)) + on(1, u=0, ux=0, uy=0)",
+        K22="int2d(Th, qft=qfHCT5)(dyy(u)*dyy(v)) + on(1, u=0, ux=0, uy=0)",
+        K26="int2d(Th, qft=qfHCT5)(dxy(u)*dyy(v) + 2.*dyy(u)*dxy(v)) + on(1, u=0, ux=0, uy=0)",
+        K66="int2d(Th, qft=qfHCT5)(2.*dxy(u)*dxy(v)) + on(1, u=0, ux=0, uy=0)",
         # Rotational inertia term
-        L="int2d(Th)(dx(u)*dx(v) + dy(u)*dy(v)) + on(1, u=0, ux=0, uy=0)",
+        L="int2d(Th, qft=qfHCT5)(dx(u)*dx(v) + dy(u)*dy(v)) + on(1, u=0, ux=0, uy=0)",
         # Mass matrix
-        M="int2d(Th)(u*v) + on(1, u=0, ux=0, uy=0)",
+        M="int2d(Th, qft=qfHCT5)(u*v) + on(1, u=0, ux=0, uy=0)",
+        # Rotational inertia term
+        LCorrection="int2d(Th, qft=qfHCT5)(indAccel*(dx(u)*dx(v) + dy(u)*dy(v))) + on(1, u=0, ux=0, uy=0)",
+        # Mass matrix
+        MCorrection="int2d(Th, qft=qfHCT5)(indAccel*(u*v)) + on(1, u=0, ux=0, uy=0)",
         # For correct usage of vectorial element
         functions=["[u, ux, uy]", "[v, vx, vy]"],
     )
@@ -100,6 +104,9 @@ def processFFOutput(ff_output: dict):
     )
     L, fL = evaluateMatrixAndRHS(ff_output["L"], f_bc)
 
+    MCorrection, fMCorrection = evaluateMatrixAndRHS(ff_output["MCorrection"], f_bc)
+    LCorrection, fLCorrection = evaluateMatrixAndRHS(ff_output["LCorrection"], f_bc)
+
     dim = M.shape[0]
     Ks = np.zeros((len(MODULI_INDICES), dim, dim))
     fKs = np.zeros((len(MODULI_INDICES), dim))
@@ -126,6 +133,10 @@ def processFFOutput(ff_output: dict):
         "fM": fM,
         "L": L,
         "fL": fL,
+        "MCorrection": MCorrection,
+        "fMCorrection": fMCorrection,
+        "LCorrection": LCorrection,
+        "fLCorrection": fLCorrection,
         "fLoad": f_load,
         "interpolation_value_from_bc": interpolation_value_from_bc,
         "interpolation_vector": interpolation_vector,